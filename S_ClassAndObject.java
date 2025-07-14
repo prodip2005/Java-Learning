ff25fcfb7f7b70e01781b262eed47e589b8ce305
Prodip Hore <prodiphore2005@gmail.com>
2025-07-14 14:26:31 +0600

    changes in S

--- S_ClassAndObject.java
+++ S_ClassAndObject.java
@@ -1,9 +1,9 @@
 class calculator{    //it is a class
-    int a;
-    public int add(int num1,int num2)    //i= what i want to return;; add=method;; public =to call from other class
+    int a;   //instant variable
+    public int add(int num1,int num2) //(method decleration)   //i= what i want to return;; add=method;; public =to call from other class
     {
-        int r=num1+num2;
-        return r;
+        int r=num1+num2; //r= local variable
+        return r; // returnning r
     }
 }
 
@@ -15,9 +15,9 @@ public class S_ClassAndObject {
         int num1=4;
         int num2=5;
 
-        calculator calc= new calculator();     //calculator=to give location of the class;calc
+        calculator calc= new calculator();  //(object creation)    //calculator=to give location of the class;calc //new=creating space in memory
         int result=calc.add(num1,num2);
-        
+
         System.out.println(result);
     }
 }
