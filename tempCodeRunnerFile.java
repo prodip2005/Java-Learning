522b0ddf893d2f28a21c04165c7a7995f99d6658
Prodip Hore <prodiphore2005@gmail.com>
2025-07-18 01:40:43 +0600

    edit

--- tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                nums[i][j]=(int)(Math.random()*10);
-                System.out.print(nums[i][j] + " ");
-            }
-            System.out.println();
-        }
