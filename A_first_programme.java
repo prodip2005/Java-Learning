a18fefc674eacccdd940f94348509edf0e03f5bb
Prodip Hore <prodiphore2005@gmail.com>
2025-07-26 18:52:11 +0600

    Update

--- A_first_programme.java
+++ A_first_programme.java
@@ -2,6 +2,6 @@ public class A_first_programme{
     public static void main(String[]args)
     {  
         System.out.println("Hello world");
-        System.out.println("Prodip")
+        System.out.println("Prodip");
     }
 }
---
+++ Z_DrawbacksOfArray.java
@@ -0,0 +1,5 @@
+public class Z_DrawbacksOfArray {
+    public static void main(String[] args) {
+        int nums[]=new int[4];
+    }
+}
